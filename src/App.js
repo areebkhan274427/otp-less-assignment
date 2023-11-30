d5fc9c51d133237198909c752ace8d2db81d2157
areebkhan274427 <areebkhan274427@gmail.com>
2023-11-30 14:14:39 +0530

    more commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [user,setUser] = useState();
 
   useEffect(() => {
     window.otpless = (otplessUser) => {
       alert(JSON.stringify(otplessUser));
+      setUser(otplessUser)
       console.log(otplessUser);
     };
   }, []);
 
   return (
     <div className="App">
+      {user && 
+      <div>
+        <h1>Welcome, {user.email.name}</h1>
+        </div>}
       <div id="otpless-login-page"></div>
+
     </div>
   );
 }
